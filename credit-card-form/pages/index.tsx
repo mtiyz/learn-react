004f7b07881ea76a58ee33fe866c769904f67371
motiya <mtiyz@users.noreply.github.com>
2020-03-30 19:57:14 +0900

    :tada: initial commit

---
+++ credit-card-form/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from '../components/Form'
+
+const Home = () => {
+  return (
+    <>
+      <h1>This is Credit Card Form</h1>
+      <Form />
+    </>
+  )
+}
+
+export default Home
