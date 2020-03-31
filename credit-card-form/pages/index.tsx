a947fbfe902e916c3c9f73cb46124df374ae5aa5
motiya <mtiyz@users.noreply.github.com>
2020-03-31 14:30:05 +0900

    :sparkles: add Emotion

--- credit-card-form/pages/index.tsx
+++ credit-card-form/pages/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
 const Index = () => {
   return (
-    <div className='h-screen flex items-center justify-center'>
-      <div>
-        Form!!!!!!!!!!!
-      </div>
+    <div className='bg-blue-200 h-screen flex items-center justify-center'>
+      <CardForm>
+        <Form className='bg-gray-100 shadow-2xl'>
+          Form!!!!!!!!!!!
+        </Form>
+      </CardForm>
     </div>
   )
 }
+
+const CardForm = styled.div`
+  height: 65vh;
+  width: 30vw;
+`
+
+const Form = styled.div`
+  border-radius: 0.75rem;
+`
 
 export default Index
